8cfa7c72acd82921bedd1ba7bed3f67e3d2b3ed6
Jason <getbest@gmail.com>
2021-03-15 15:21:39 -0400

    express server netlify test

---
+++ src/api.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    name: "Jason",
+    residence: "Canada",
+    character: "brilliant"
+
+
+  });
+});
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
